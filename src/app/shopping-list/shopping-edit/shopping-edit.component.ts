55025853c38dc26a57ce3eb9ed6e498540c71481
Oleksii Borysonok <alex.borysonok@gmail.com>
2018-11-18 22:05:52 -0500

    now use ViewChild for adding a new item

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,13 +7,18 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('inputName') inputName: ElementRef;
+  @ViewChild('inputAmount') inputAmount: ElementRef;
   @Output() onAddNewItem = new EventEmitter;
+
   constructor() { }
 
   ngOnInit() { }
 
-  public addNewItem(name, amount) {
-    console.log("===== new item: ", name, amount);
+  public addNewItem() {
+    // console.log("===== new item: ", name, amount);
+    const name = this.inputName.nativeElement.value;
+    const amount = this.inputAmount.nativeElement.value;
     const newItem = { name: name, amount: amount };
     this.onAddNewItem.emit(newItem);
   }
